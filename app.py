5ec817382b3a2b44b92cae40202a704056443a13
JacobStudstill <jacob.studstill@gmail.com>
2023-10-02 21:15:38 -0400

    Added comments to the code and added empty arrays to push data into when the loop is written

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from pathlib import Path
 import os
 
+# Empty Arrays that will be used to push files into
+documents = []
+pictures = []
+music = []
+videos = []
+applications = []
+
+# Get the download path file of the user
 downloads_path = str(Path.home() / "Downloads")
+
+# Ensure that the download path is pointing to the right place
+print(downloads_path)
+
+# Assign the files of the directory to a variable
 list = os.listdir(downloads_path)
 
-# open(downloads_path)
+#Print out the current list of files in the download directory
+print(list)
 
-# print (downloads_path.readable())
+
